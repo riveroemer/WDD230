1e1b0fe691a29e281d96276ab0cd96bcd18c174f
EmerRivero <riv21017@byui.edu>
2022-10-07 19:36:23 -0500

    update

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -23,13 +23,12 @@ dateEl.innerHTML = `${curDay()}, ${curDayofMonth} ${curMonth()} ${curYear}`;
 
 footerEl.innerHTML = `&copy${curYear} Merida`
 
-const lastModifiedDate = new Date(document.lastModified)
-
-let modYear = lastModifiedDate.getFullYear();
-let modDay = lastModifiedDate.getDay();
-let modMonth = lastModifiedDate.getMonth();
-let modminute = lastModifiedDate.getMinutes();
-let modsecond = lastModifiedDate.getSeconds()
-let modhour = lastModifiedDate.getHours(); 
-
-modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`
+const d = new Date();
+const last_updated = document.querySelector(".last_updated");
+const current_year = document.querySelector(".current_year");
+last_updated.textContent = document.lastModified;
+current_year.textContent = d.getFullYear();
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	d);
